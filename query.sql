89ffa40022d6872c695cb285a838fb65804cc3bb
Hosne Mubarak <hosnemubarak93@gmail.com>
2025-09-28 19:03:38 +0600

    Add files via upload

---
+++ query.sql
@@ -0,0 +1,30 @@
+
+1.ANSWER  
+SELECT * FROM posts ; 
+
+2.ANSWER
+SELECT posts.title, users.name AS author
+FROM posts
+INNER JOIN users ON posts.author_id = users.id
+
+3.ANSWER
+INSERT INTO posts (title,slug, body, author_id)
+VALUES("Fourth post", "fourth_post", "This is fourth post", 2 )
+
+4.ANSWER
+UPDATE posts SET title="Now Updated first title" WHERE id=1;
+
+5.ANSWER
+SELECT users.name, COUNT(posts.id) AS total_post
+FROM users
+LEFT JOIN posts ON users.id = posts.author_id
+GROUP BY users.id ;
+
+6.ANSWER
+SELECT * FROM posts ORDER BY created_at DESC;
+
+7.ANSWER
+SELECT * FROM posts
+ORDER BY created_at
+DESC
+LIMIT 2 ;
